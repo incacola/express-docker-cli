3e3b2a2691a6a47c9223fbb5c781c55203cb675d
Felipe Gil Gutierrez <felipe.gil@arvato.com>
2018-07-18 17:06:29 +0200

    CLI: enquirer questions added

--- index.js
+++ index.js
@@ -2,22 +2,27 @@
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
-const execa = require('execa')
-const Listr = require('listr')
-const resolveGlobal = require('resolve-global')
-const inquirer = require('inquirer')
+const execa = require('execa');
+const Listr = require('listr');
+const resolveGlobal = require('resolve-global');
+const Enquirer = require('enquirer');
+const enquirer = new Enquirer();
+
+enquirer.register('confirm', require('prompt-confirm'));
 
 var ejs = require('ejs')
 var fs = require('fs')
 var minimatch = require('minimatch')
 var mkdirp = require('mkdirp')
 var path = require('path')
-var program = require('commander')
-var readline = require('readline')
 var sortedObject = require('sorted-object')
 var util = require('util')
 
- 
+ /***
+  * 
+  * clear the console & write with figlet the CLI Tool Name
+  * 
+ */
 clear();
 console.log(
   chalk.blue.bold(
@@ -26,51 +31,27 @@ console.log(
     })
   )
 );
-
+/*** 
+ * Start Enquirer to 
+*/
 let expressString = 'express '
 
-inquirer.prompt([
+const questions = [
   {
-    name: 'expressDir',
-    type: 'input',
-    message: 'Where would you like to unfold your? [default: current folder]',
-    default: './'
+    type: 'confirm',
+    name: 'chocolate',
+    message: 'Like chocolate?'
   },
   {
-
+    type: 'confirm',
+    name: 'vanilla',
+    message: 'Like vanilla?'
   }
-]).then(answers => {
+];
 
+enquirer.ask(questions)
+.then(function(answers) {
+  console.log(answers)
 });
 
-new Listr([
-  {
-    title: 'Installing & unfolding express-generator',
-    task: () => {
-      if(resolveGlobal.silent('express')){
-        execa.shell('npm install -g express-generator')
-        execa.shell('express --view=hbs ./tmp')
-      }else{
-        execa.shell('express --view=hbs ./tmp')
-      }
-    }
-  },
-  {
-    title: 'Removing package-lock',
-    task: () => execa('rm', ['package-lock.json'])
-  },
-  {
-    title: 'Running npm install',
-    task: () => execa('npm', ['install'])
-  },
-  {
-    title: 'Adding package-lock to git',
-    task: (ctx, task) => execa('git', ['add', 'package-lock.json']).catch(()=> task.skip())
-  },
-  {
-    title: 'Input test',
-    task: () =>{
-      
-    }
-  }
-]).run();
+
